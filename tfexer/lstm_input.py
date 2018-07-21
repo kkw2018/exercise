e0308cbc55b7da6bee05e21be10ba24452d0d3e0
kkw2018 <240116906@qq.copm>
2018-07-21 21:48:47 +0800

    little change

--- tfexer/lstm_input.py
+++ tfexer/lstm_input.py
@@ -104,34 +104,18 @@ if not isfile("labels.npy"):
     getlabels(posfile, negfile)
 labels = np.load("labels.npy")
 
-dataframe = pd.DataFrame(ids)
-print(dataframe.describe())
+features = {'ids':ids,'lens':lens}
+
 def my_input_fn(features, targets, batch_size=1, shuffle=True, num_epochs=None):
-    """Trains a linear regression model of one feature.
 
-    Args:
-      features: pandas DataFrame of features
-      targets: pandas DataFrame of targets
-      batch_size: Size of batches to be passed to the model
-      shuffle: True or False. Whether to shuffle the data.
-      num_epochs: Number of epochs for which data should be repeated. None = repeat indefinitely
-    Returns:
-      Tuple of (features, labels) for next data batch
-    """
-
-    # Convert pandas data into a dict of np arrays.
     features = {key: np.array(value) for key, value in dict(features).items()}
-
-    # Construct a dataset, and configure batching/repeating
     ds = Dataset.from_tensor_slices((features, targets))  # warning: 2GB limit
+
     ds = ds.batch(batch_size).repeat(num_epochs)
-
-    # Shuffle the data, if specified
     if shuffle:
-        ds = ds.shuffle(10000)
-
-    # Return the next batch of data
-    features, labels = ds.make_one_shot_iterator().get_next()
-    return features, labels
-
+        ds.shuffle(buffer_size=60000)
+    features,labels = ds.make_one_shot_iterator().get_next()
+    print(features,labels)
+    return features,labels
+my_input_fn(features,labels)
 
